6d27c1dc37f88f783876f250a2ffd503da9d70c8
GregV3 <grzegorz.muszynski77@gmail.com>
2024-11-18 20:09:33 +0100

    chapter 14 completed

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -73,8 +73,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
           <div id="amount-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                 </p>
@@ -96,6 +96,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
+
                 />
                 <label
                   htmlFor="pending"
@@ -122,6 +124,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </fieldset>
+        {state.errors?.status &&
+          state.errors.status.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+        ))}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
